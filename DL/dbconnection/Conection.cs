ce57816a6c84bac390695ff61c42ca06ce0ca001
burundur9 <misbakhovdanis2005@gmail.com>
2025-02-19 10:39:35 +0300

    Добавьте файлы проекта.

---
+++ DL/dbconnection/Conection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DL.dbconnection
+{
+  public  class Conection
+    {
+        public static LibEntities biblioteq= new LibEntities();
+    }
+}
