c96b129830a0b2991205a43935d652e339bcfe44
burundur9 <misbakhovdanis2005@gmail.com>
2025-02-19 10:43:39 +0300

    сообщение

--- DL/dbconnection/Conection.cs
+++ DL/dbconnection/Conection.cs
@@ -6,8 +6,9 @@ using System.Threading.Tasks;
 
 namespace DL.dbconnection
 {
-  public  class Conection
+  public   class Conection
     {
-        public static LibEntities biblioteq= new LibEntities();
+
+        public static LibEntities biblioteq = new  LibEntities() ;
     }
 }
